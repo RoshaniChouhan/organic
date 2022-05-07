cb42370d83b2f7e54243b33a4f5a71d5cc9248f9
RoshaniChouhan <your github email>
2022-05-07 19:37:58 +0530

    skin

---
+++ skincare.js
@@ -0,0 +1,232 @@
+var bestsellar = [
+    {
+       img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/GLOWNER-01_e0fa1e78-3b5f-4fbb-a10d-12f90cf9788d_700x.jpg?v=1647499518",
+    
+      name:"Glowner - Face Toner",
+        des:"Refreshing, Hydrating & Skin Brightening",
+        price:"RS 220/-",
+        
+    },
+
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/C-GlowFaceToner-01_915bc29e-a316-4e20-b70c-268f78fc5ce4_700x.jpg?v=1651561057",
+       name:"C-Glow Face Toner - 100ml",
+         des:"Clarifying & Soothing",
+         price:"RS 299/-",
+         
+     },
+
+     {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/TheHydrationNationCombo-01_700x.jpg?v=1651698406",
+     
+       name:"The Hydration Nation Combo",
+         des:"C-Glow Face Wash, C-Glow Face Cream & C-Glow Face Serum",
+         price:"RS 285/-",
+         
+     },
+
+     {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Anti-AcneFaceGel-01_0b665e08-c71f-409a-82ef-10b68c67e453_700x.jpg?v=1651699935",
+     
+       name:"Anti-Acne Face Gel",
+         des:"Pimple Scar Removal & Skin Clarifying",
+         price:"RS 375/-",
+         
+     },
+
+
+     {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/GrowBrow-01_360x.jpg?v=1651216876",
+       name:"GrowBrow - Eyebrow & Eyelash Oil",
+         des:"Thicker & Stronger Eyebrows",
+         price:"RS 330/-",
+         
+     },
+
+     {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/LipBalm_-NicoLips-Lip-Scrub-Combo_700x.jpg?v=1647863372",
+     
+       name:"Lip Love Combo",
+         des:"Nicolips & The Lip Balm",
+         price:"RS 538/-",
+         
+     },
+     {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Kumkumadi-Face-Oil-01_1_700x.jpg?v=1651698324",
+     
+       name:"Kumkumadi Face Oil",
+         des:"Skin Brightening & Hydrating For Youthful Skin",
+         price:"RS 549/-",
+         
+     },
+
+     {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/GLOWNER-01_e0fa1e78-3b5f-4fbb-a10d-12f90cf9788d_700x.jpg?v=1647499518",
+       name:"Glowner - Face Toner",
+         des:"Refreshing, Hydrating & Skin Brightening",
+         price:"RS 240/-",
+         
+     },
+     {
+         img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Glowey-01_700x.jpg?v=1649143813",
+         name:"Glowey - Face Pack",
+         des:"Deep Cleansing, Hydrating & Skin Reviving",
+         price:"RS 375/-",
+     },
+     {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Coalwave-01_360x.jpg?v=1651696320",
+        name:"CoalWave Body Wash Bar",
+        des:"Exfoliating & Deeply Cleansing",
+        price:"RS 475/-",
+    },
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Exfoliate-01_1_360x.jpg?v=1651698280",
+        name:"Exfoliate Face & Body Scrub",
+        des:"Dirt Removal, Nourishing & Skin Brightening",
+        price:"RS 475/-"
+    
+    },
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Box-2-03_360x.jpg?v=1651474038",
+        name:"Everyday Skincare Essentials Mother’s Day Gift Box",
+        des:"Everyday Skincare Essentials Mother’s Day Gift Box",
+        price:"RS 649/-",
+    },
+   
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/KumkumadiFaceWash-01_1_360x.jpg?v=1651698324",
+        name:"Kumkumadi Face Wash",
+        des:"Pore Cleansing, Radiating & Skin Rejuvenating",
+        price:"RS 579/-",
+    },
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/SupremeWomenCombo-EyeLift_CGlowFacewash_Exfoliate_DeoWhite-Woen_Nicolips_360x.jpg?v=1649311611",
+        name:"Supreme Women Combo",
+        des:"Eye Lift, C-Glow Face Wash , Exfoliate Scrub, Deo White - Women, Nicolips",
+        price:"RS 1050/-",
+    },
+
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Supreme-Men-Combo-01_360x.jpg?v=1651698398",
+        name:"Supreme Men Combo",
+        des:"Eye Lift, C Glow Face Wash , Exfoliate , Deo White - Men & Nicolips",
+        price:"RS 999/-",
+    },
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/C-GlowFacePack-01_1_360x.jpg?v=1651698247",
+        name:"C-Glow Face Pack",
+        des:"Skin Brightening & Anti Blemish",
+        price:"RS 349/-",
+    },
+
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/C-GlowFacePack-01_1_360x.jpg?v=1651698247",
+        name:"C-Glow Face Pack",
+        des:"Skin Brightening & Anti Blemish",
+        price:"RS 349/-",
+    },
+
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/DayGlow-01_360x.jpg?v=1648114071",
+        name:"Day Glow Sunscreen SPF 30+",
+        des:"Face, Neck & Body Lotion",
+        price:"RS 449/-",
+    },
+
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/DeTox-01_360x.jpg?v=1651696345",
+        name:"Detox Body Wash Bar",
+        des:"Deeply Cleansing & Hydrating",
+        price:"RS 149/-",
+    },
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Oil-Control-Face-Wash-01_360x.jpg?v=1651698352",
+        name:"Oil-Control Face Wash",
+        des:"Deep Cleansing And Skin Brightening",
+        price:"RS 249/-",
+    },
+
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Anti-AcneFaceGel-01_0b665e08-c71f-409a-82ef-10b68c67e453_360x.jpg?v=1651699935",
+        name:"Anti-Acne Face Gel",
+        des:"Skin Brightening & Anti Blemish",
+        price:"RS 249/-",
+    },
+
+    {
+        img:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Glowtion-01_cab54ba7-e00c-4426-87c1-249ee10304c6_360x.jpg?v=1651571277",
+        name:"Face & Body Lotion",
+       
+      
+        des:"Moisturizing & Nourishing",
+        price:"RS 275/-",
+    },
+
+
+
+
+
+
+
+];
+
+
+
+
+
+
+
+
+localStorage.setItem("best",JSON.stringify(bestsellar));
+
+ var cart=JSON.parse(localStorage.getItem("best"))||[];
+
+var product=document.querySelector(".bestsellar");
+function display(data){
+    product.innerHTML=null;
+
+    data.forEach(function(elem){
+var pro=document.createElement("div");
+var img=document.createElement("img");
+img.src=elem.img;
+var name=document.createElement("h2");
+name.innerText=elem.name;
+
+var des=document.createElement("p");
+des.innerText=elem.des;
+
+var price=document.createElement("h2");
+price.innerText=elem.price;
+ var btn=document.createElement("button");
+ btn.innerText="ADD TO CART";
+ btn.addEventListener("click",function(){
+     AddTocart(elem);
+ })
+
+pro.append(img,name,des,price,btn);
+
+product.append(pro);
+
+    });
+
+}
+
+
+display(bestsellar);
+// filter.addEventListener("change",function(){
+//    if(filter.value=="none"){
+//        display(bestsellar)
+//    }else{
+//        var filtered=bestsellar.filter(function(elem){
+// return elem.price==filter.value;
+//        })
+
+//        display(filtered);
+//    } 
+// })
+function AddTocart(elem){
+cart.push(elem);
+localStorage.setItem("best",JSON.stringify(cart));
+
+alert("Add to product")
+}
