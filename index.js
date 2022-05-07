7c5f211fd8e62da664f9126ba2722f2581c55a4f
RoshaniChouhan <your github email>
2022-05-07 19:33:04 +0530

    po

---
+++ index.js
@@ -0,0 +1,22 @@
+var searchForm = document.querySelector(".search-form");
+document.querySelector("#search-btn").onclick = () =>{
+    searchForm.classList.toggle("active");
+
+}
+
+
+
+var shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#cart-btn").onclick = () =>{
+    shoppingCart.classList.toggle("active");
+
+}
+
+
+
+
+var loginForm = document.querySelector(".login-form");
+document.querySelector("#login-btn").onclick = () =>{
+    loginForm.classList.toggle("active");
+
+}
